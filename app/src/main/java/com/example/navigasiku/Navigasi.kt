f39c8f5145c6c1e6dd27aa41386eb76d4736c405
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-06 09:13:17 +0700

    menambahkan enum class Navigasi

---
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -0,0 +1,29 @@
+package com.example.navigasiku
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi{
+    Formulir,
+
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavController= rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController
+        )
+
+    }
+}
